002ae1596754cc7335ccc4f161815c61243f98e6
sndrgb <alessandro@sixsocks.studio>
2021-05-25 13:06:01 +0200

    new features

--- basic/shell/more/aliases.sh
+++ basic/shell/more/aliases.sh
@@ -4,6 +4,8 @@ alias cls='clear' # Good 'ol Clear Screen command
 
 alias ssh-unlock='ssh-add ~/.ssh/*'
 
+echo "aliases"
+
 if [ -x "$(command -v exa)" ]; then
     alias ls="exa"
     alias la="exa --long --all --group"
